8c9cc098d12781639f040d3a8aa5b1e9469d9bde
Deependra Kumar <keshrideependra17@gmail.com>
2025-12-02 09:44:16 +0530

    Time: 1 ms (90.6%), Space: 45.6 MB (18.33%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        boolean isVisted[] =new boolean[nums.length];
-        helper(nums,ans, list, isVisted);
+        helper(nums, 0,ans);
         return ans;
     }
-    public void helper(int nums[], List<List<Integer>> ans, List<Integer> list, boolean isVisted[]){
-        if(list.size()==nums.length){
-            List<Integer> l = new ArrayList<>();
-            for(int i = 0; i < list.size(); i++){
-                l.add(list.get(i));
+    public void helper(int nums[], int idx, List<List<Integer>> ans){
+        if(idx==nums.length-1){
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<nums.length; i++){
+                list.add(nums[i]);
             }
-            ans.add(l);
+            ans.add(list);
             return;
         }
-        for(int i=0; i<nums.length; i++){
-            if(isVisted[i]==false){
-                isVisted[i]=true;
-                list.add(nums[i]);
-                helper(nums,ans,list,isVisted);
-                isVisted[i]=false;
-                list.remove(list.size()-1);
-            }
+        for(int i=idx; i<nums.length; i++){
+            swap(nums, i, idx);
+            helper(nums, idx+1, ans);
+            swap(nums, i, idx);
         }
+    }
+    public void swap(int nums[], int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
