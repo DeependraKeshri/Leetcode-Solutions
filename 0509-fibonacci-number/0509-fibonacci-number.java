68fba8b2309c2d84d6fa653a02d9af4b8704e4ee
Deependra Kumar <keshrideependra17@gmail.com>
2025-12-31 12:56:30 +0530

    Time: 10 ms (24.89%), Space: 42.3 MB (12.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,6 @@
 class Solution {
-    //Memoiation
-    public int fibo(int n, int arr[]){
-        if(n==0 || n==1)return n;
-        if(arr[n]!=-1)return arr[n];
-        arr[n]=fibo(n-1,arr)+fibo(n-2,arr);
-        return arr[n];
-    }
     public int fib(int n) {
-        int arr[]=new int[n+1];
-        Arrays.fill(arr,-1);
-        return fibo(n,arr);
+        if(n==0 || n==1)return n;
+        return fib(n-1)+fib(n-2);
     }
 }
