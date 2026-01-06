ff712eca82107f4b911ebd7be4366738e4d6788b
Deependra Kumar <keshrideependra17@gmail.com>
2026-01-06 10:16:37 +0530

    Time: 9 ms (64.42%), Space: 49.5 MB (17.48%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -14,31 +14,26 @@
  * }
  */
 class Solution {
-    public int level(TreeNode root, int n) {
-        if (root == null) return 0;
-
-        if (n == 1) return root.val;
-
-        return level(root.left, n - 1) 
-             + level(root.right, n - 1);
-    }
-
     public int maxLevelSum(TreeNode root) {
-        int h = height(root);
-        int maxSum = Integer.MIN_VALUE;
-        int idx = 1;
-
-        for (int i = 1; i <= h; i++) {
-            int sum = level(root, i);
-            if (sum > maxSum) {
-                maxSum = sum;
-                idx = i;
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root!=null)q.add(root);
+        int level=1, ans=1;
+        int maxSum=Integer.MIN_VALUE;
+        while(!q.isEmpty()){
+            int size=q.size();
+            int sum=0;
+            for(int i=0; i<size; i++){
+                TreeNode t=q.poll();
+                sum+=t.val;
+                if(t.left!=null)q.offer(t.left);
+                if(t.right!=null)q.offer(t.right);
             }
+            if(maxSum<sum){
+                maxSum=sum;
+                ans=level;
+            }
+            level++;
         }
-        return idx;
-    }
-    public int height(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(height(root.left), height(root.right)) + 1;
+        return ans;
     }
 }
