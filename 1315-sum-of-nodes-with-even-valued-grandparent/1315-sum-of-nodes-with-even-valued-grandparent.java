221aa0eb07e4fad85429a9b2d92604b830a91687
Deependra Kumar <keshrideependra17@gmail.com>
2026-01-18 11:26:54 +0530

    Time: 1 ms (99.64%), Space: 47.9 MB (35.24%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,25 +14,21 @@
  * }
  */
 class Solution {
+    int sum=0;
     public int sumEvenGrandparent(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int sum=0;
-        while(!q.isEmpty()){
-            TreeNode t=q.remove();
-            if(t.left!=null)q.add(t.left);
-            if(t.right!=null)q.add(t.right);
-            if(t.val%2==0){
-                if(t.left!=null){
-                    if(t.left.left!=null)sum+=t.left.left.val;
-                    if(t.left.right!=null)sum+=t.left.right.val;
-                }
-                if(t.right!=null){
-                    if(t.right.left!=null)sum+=t.right.left.val;
-                    if(t.right.right!=null)sum+=t.right.right.val;
-                }
+        if(root==null)return 0;
+        if(root.val%2==0){
+            if(root.left!=null){
+                if(root.left.left!=null)sum+=root.left.left.val;
+                if(root.left.right!=null)sum+=root.left.right.val;
+            }
+            if(root.right!=null){
+                if(root.right.left!=null)sum+=root.right.left.val;
+                if(root.right.right!=null)sum+=root.right.right.val;
             }
         }
+        sumEvenGrandparent(root.left);
+        sumEvenGrandparent(root.right);
         return sum;
     }
 }
