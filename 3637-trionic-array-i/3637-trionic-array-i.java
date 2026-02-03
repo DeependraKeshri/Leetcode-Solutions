b0ddebd4232f6379ab70765654c4c31486c50bd3
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-03 17:56:09 +0530

    Time: 1 ms (72.27%), Space: 44.8 MB (31.09%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,36 +1,38 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int i=1;
-        int n=nums.length;
-        while(i<n){
-            if(nums[i-1]<nums[i]){
-                i++;
-            }else if(i==1 || i==n-1){
+        boolean point1 = false;
+        boolean point2 = false;
+        int pt1 = -1;
+        int pt2 = -1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return false;
-            }else{
-                int j=i;
-                while(j<n){
-                    if(nums[j-1]>nums[j]){
-                        j++;
-                    }else if(j==n-1 && nums[j-1]>nums[j]){
-                        return false;
-                    }else{
-                        int k=j;
-                        while(k<n){
-                            if(k==n-1 && nums[k-1]<nums[k])return true;
-                            else if(nums[k-1]<nums[k]){
-                                k++;
-                            }else{
-                                return false;
-                            }
-                            i++;
-                            j++;
-                        }
-                    }
-                    i++;
+            }
+        }
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+                if (pt1 == -1) {
+                    pt1 = i;
+                    point1 = true;
+                } else {
+                    return false;
+                }
+            } else if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) {
+                if (pt2 == -1) {
+                    pt2 = i;
+                    point2 = true;
+                } else {
+                    return false;
                 }
             }
         }
-        return false;
+
+        if (point1 && point2 && pt1 < pt2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
