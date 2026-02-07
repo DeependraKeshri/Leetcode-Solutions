8e7a92fd39c1d1e8c9896f41d957051524381c59
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-07 10:31:41 +0530

    Time: 18 ms (91.13%), Space: 47.3 MB (91.61%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,24 +1,52 @@
 class Solution {
     public String sortVowels(String s) {
-        ArrayList<Character> list=new ArrayList<>();
+        int idx[]=new int[10];
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U'){
-                list.add(ch);
+            if(ch=='A'){
+                idx[0]+=1;
+            }else if(ch=='E'){
+                idx[1]+=1;
+            }else if(ch=='I'){
+                idx[2]+=1;
+            }else if(ch=='O'){
+                idx[3]+=1;
+            }else if(ch=='U'){
+                idx[4]+=1;
+            }else if(ch=='a'){
+                idx[5]+=1;
+            }else if(ch=='e'){
+                idx[6]+=1;
+            }else if(ch=='i'){
+                idx[7]+=1;
+            }else if(ch=='o'){
+                idx[8]+=1;
+            }else if(ch=='u'){
+                idx[9]+=1;
             }
         }
-        Collections.sort(list);
-        String ans="";
+        StringBuilder sb=new StringBuilder();
+        StringBuilder st=new StringBuilder();
+
+        String temp="AEIOUaeiou";
+        for(int i=0; i<idx.length; i++){
+            int val=idx[i];
+            while(val>0){
+                st.append(temp.charAt(i));
+                val--;
+            }
+        }
+
         int j=0;
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
             if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U'){
-                ans+=list.get(j);
+                sb.append(st.charAt(j));
                 j++;
             }else{
-                ans+=ch;
+                sb.append(ch);
             }
         }
-        return ans;
+        return sb.toString();
     }
 }
