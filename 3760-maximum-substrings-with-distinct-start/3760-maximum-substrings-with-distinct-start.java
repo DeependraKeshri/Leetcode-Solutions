12dafd5b96ccbff4a16ae47fae713ae974f972e4
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-13 09:35:51 +0530

    Time: 5 ms (98.98%), Space: 48.1 MB (15.43%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -3,8 +3,9 @@ class Solution {
         boolean arr[]=new boolean[26];
         int count=0;
         for(int i=0; i<s.length(); i++){
-            if(!arr[s.charAt(i)-'a']){
-                arr[s.charAt(i)-'a']=true;
+            int idx=s.charAt(i)-'a';
+            if(!arr[idx]){
+                arr[idx]=true;
                 count++;
             }
         }
