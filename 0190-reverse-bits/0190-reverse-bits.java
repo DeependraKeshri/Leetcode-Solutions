8fecd77bbb8136bde45588d99ec47deef5e7e661
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-16 12:38:30 +0530

    Time: 1 ms (60.34%), Space: 42.5 MB (52.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,20 +1,13 @@
 class Solution {
     public int reverseBits(int n) {
-        StringBuilder sb=new StringBuilder(Integer.toBinaryString(n));
-        while (sb.length() < 32) {
-            sb.insert(0, '0');
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
         }
-        int s=0, e=sb.length()-1;
-        while(s<e){
-            char f=sb.charAt(s);
-            char l=sb.charAt(e);
-            sb.setCharAt(s,l);
-            sb.setCharAt(e,f);
-            s++;
-            e--;
-        }
-        String str=sb.toString();
-        int val= Integer.parseInt(str,2);
-        return val;
+
+        return result;
     }
 }
