74bfd30305881c939cf45c679451b7156562f660
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-17 14:09:26 +0530

    Time: 1 ms (99.92%), Space: 45.8 MB (13.76%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -13,19 +13,25 @@
  *     }
  * }
  */
- class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    List<Integer> list = new ArrayList<>();
-
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        if (root == null) return ans;
-        list.add(root.val);
-        if (root.left == null && root.right == null && targetSum == root.val) {
-            ans.add(new ArrayList<>(list)); 
+class Solution {
+    public void helper(TreeNode root, List<List<Integer>> l,List<Integer> lt, int targetSum){
+        if(root==null)return;
+        lt.add(root.val);
+        if(root.left==null && root.right==null){
+            if(targetSum-root.val==0){
+                l.add(new ArrayList(lt));
+            }
+            lt.remove(lt.size()-1);
+            return;
         }
-        pathSum(root.left, targetSum - root.val);
-        pathSum(root.right, targetSum - root.val);
-        list.remove(list.size() - 1);
-        return ans;
+        helper(root.left, l, lt, targetSum-root.val);
+        helper(root.right, l, lt, targetSum-root.val);
+        lt.remove(lt.size()-1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> li=new ArrayList<>();
+        helper(root, list, li, targetSum);
+        return list;
     }
 }
