b2e263bcd8048267e070eceb457b40f5e2d97cd1
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-18 12:42:18 +0530

    Time: 287 ms (5.02%), Space: 48.2 MB (39.2%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        ArrayList<Integer> list=new ArrayList<>();
         for(int val:nums){
-            if(!set.contains(val))set.add(val);
-            else set.remove(val);
+            if(!list.contains(val))list.add(val);
+            else list.remove(Integer.valueOf(val));
         }
         int ans[]=new int[2];
-        for(int val:set){
+        for(int val:list){
             if(ans[0]==0)ans[0]=val;
             else ans[1]=val;
         }
