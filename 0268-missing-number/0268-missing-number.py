1f0e65f35f4503ad93fe6ee63afb585ad3cfad49
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-20 23:48:16 +0530

    Time: 4 ms (47.11%), Space: 20.7 MB (15.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        import numpy as np
-        
-        n = len(nums)
-        ar = np.arange(n + 1)
-        
-        st = set(nums)
-        st1 = set(ar)
-        
-        num =int((st1.symmetric_difference(st)).pop())
-        return num
+        xor=0
+        for i in range(len(nums)+1):
+            xor^=i
+        for i in nums:
+            xor^=i
+        return xor
