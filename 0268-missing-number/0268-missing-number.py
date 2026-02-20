9334049cf19bb1657ede87e6d455ff8a22dfd56d
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-20 23:52:16 +0530

    Time: 3 ms (62.39%), Space: 20.5 MB (57.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(0, len(nums)+1):
-            if i not in nums:
-                return i
+        n=len(nums)
+        s=(int)((n/2)*(n+1))
+        return s-sum(nums)
