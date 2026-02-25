ca20d6dd88b68ad7b8a3ebfb232ca125e54cb202
Deependra Kumar <keshrideependra17@gmail.com>
2026-02-25 14:46:38 +0530

    Time: 1 ms (99.92%), Space: 46.8 MB (64.23%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.java
+++ 3668-restore-finishing-order/3668-restore-finishing-order.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
         int n=friends.length;
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0; i<n; i++){
-            set.add(friends[i]);
-        }
+        int ans[]=new int[n];
         int j=0;
         for(int i=0; i<order.length; i++){
-            if(set.contains(order[i]))friends[j++]=order[i];
+            for(int k=0; k<friends.length; k++){
+                if(order[i]==friends[k]){
+                    ans[j++]=order[i];
+                    break;
+                }
+            }
         }
-        return friends;
+        return ans;
     }
 }
