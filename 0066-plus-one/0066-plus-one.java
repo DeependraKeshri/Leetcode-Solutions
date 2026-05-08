6bb0d01ab60e1b80fb1ca8ff0ddf6e7ad43a426d
Deependra Kumar <keshrideependra17@gmail.com>
2026-05-09 00:09:01 +0530

    Time: 1 ms (3.82%), Space: 42.9 MB (98.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,28 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int val=digits[digits.length-1]+1;
-        int carry=val/10;
-        ListNode temp=new ListNode(val%10);
+        Stack<Integer> st=new Stack<>();
+        int v=digits[digits.length-1]+1;
+        st.push(v%10);
+        int carry=v/10;
         for(int i=digits.length-2; i>=0; i--){
-            int v=digits[i]+carry;
-            carry=v/10;
-            ListNode t=new ListNode(v%10);
-            t.next=temp;
-            temp=t;
+            int val=digits[i]+carry;
+            st.push(val%10);
+            carry=val/10;
         }
-        int size=digits.length;
-        if(carry==1){
-            ListNode t=new ListNode(1);
-            t.next=temp;
-            temp=t;
-            size++;
-        }
-        ListNode dummy=temp;
-        int ans[]=new int[size];
+        if(carry==1)st.push(1);
+        int ans[]=new int[st.size()];
         int i=0;
-        while(dummy!=null){
-            ans[i++]=dummy.val;
-            dummy=dummy.next;
+        while(!st.isEmpty()){
+            ans[i++]=st.pop();
         }
         return ans;
     }
