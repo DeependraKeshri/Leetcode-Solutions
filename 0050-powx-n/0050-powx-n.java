28108ee7c80252ccb20e2cbf8f7755de863f9c9d
Deependra Kumar <keshrideependra17@gmail.com>
2026-06-18 12:08:02 +0530

    Time: 1 ms (9.76%), Space: 48.2 MB (33.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private double power(double x, long n) {
+        if (n == 0) return 1;
+
+        double val = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return val * val;
+        } else {
+            return val * val * x;
+        }
     }
 }
