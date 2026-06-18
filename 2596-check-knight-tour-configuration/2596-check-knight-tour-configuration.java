a8a99a2a3b5f0c9952f5fef1d11150d3649be525
Deependra Kumar <keshrideependra17@gmail.com>
2026-06-18 13:58:01 +0530

    Time: 1 ms (96.82%), Space: 46.5 MB (13.46%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -3,41 +3,34 @@ class Solution {
         if(grid[0][0]!=0)return false;
         return helper(grid, 0,0,0);
     }
-    public boolean helper(int [][]grid, int rows, int col, int num){
+    public boolean helper(int grid[][], int row, int col, int num){
         int n=grid.length;
-        if(grid[rows][col]==n*n-1)return true;
-        //top-left
-        int i=rows-2;
-        int j=col-1;
-        if(i>-1 && j>-1 && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //top-right
-        i=rows-2;
-        j=col+1;
-        if(i>-1 && j<n && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //bottom-left
-        i=rows+2;
+        if(grid[row][col]==n*n-1)return true;
+        int i,j;
+        i=row-1;
+        j=col-2;
+        if(i>=0 && j>=0 && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row-2;
         j=col-1;
-        if(i<n && j>-1 && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //bottom-right
-        i=rows+2;
+        if(i>=0 && j>=0 && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row-2;
         j=col+1;
-        if(i<n && j<n && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //mid-top-left
-        i=rows-1;
-        j=col-2;
-        if(i>-1 && j>-1 && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //mid-bottom-left
-        i=rows+1;
-        j=col-2;
-        if(i<n && j>-1 && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //mid-top-right
-        i=rows-1;
+        if(i>=0 && j<n && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row-1;
         j=col+2;
-        if(i>-1 && j<n && grid[i][j]==num+1) return helper(grid, i, j, num+1);
-        //mid-bottom-right
-        i=rows+1;
+        if(i>=0 && j<n && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row+1;
         j=col+2;
-        if(i<n && j<n && grid[i][j]==num+1) return helper(grid, i, j, num+1);
+        if(i<n && j<n && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row+2;
+        j=col+1;
+        if(i<n && j<n && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row+2;
+        j=col-1;
+        if(i<n && j>=0 && grid[i][j]==num+1)return helper(grid, i, j, num+1);
+        i=row+1;
+        j=col-2;
+        if(i<n && j>=0 && grid[i][j]==num+1)return helper(grid, i, j, num+1);
         return false;
     }
 }
