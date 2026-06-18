260feba8fef7ca68532892889c4044c29f0f5635
Deependra Kumar <keshrideependra17@gmail.com>
2026-06-18 11:07:31 +0530

    Time: 34 ms (10.78%), Space: 47.2 MB (6.46%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    HashSet<List> set=new HashSet<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list=new ArrayList<>();
+        helper(nums, 0, list);
+        return list;
+    }
+    public void swap(int nums[], int i, int idx){
+        if(i==idx)return;
+        int temp=nums[i];
+        nums[i]=nums[idx];
+        nums[idx]=temp;
+    }
+    public void helper(int nums[], int idx, List<List<Integer>> list){
+        if(idx==nums.length-1){
+            List<Integer> l=new ArrayList<>();
+            for(int i=0; i<nums.length; i++){
+                l.add(nums[i]);
+            }
+            if(!set.contains(l))list.add(l);
+            set.add(l);
+            return;
+        }
+        for(int i=idx; i<nums.length; i++){
+            swap(nums, i, idx);
+            helper(nums, idx+1, list);
+            swap(nums,i, idx);
+        }
+    }
+}
