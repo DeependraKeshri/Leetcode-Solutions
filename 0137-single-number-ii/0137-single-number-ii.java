06379abb24dc5fcebb599e99790be0ced7578461
Deependra Kumar <keshrideependra17@gmail.com>
2026-07-25 19:13:20 +0530

    Time: 7 ms (20.54%), Space: 46 MB (30.78%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,13 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-        for(int i=0; i<32; i++){
-            int count=0;
-            for(int j=0; j<nums.length; j++){
-                if((nums[j] & (1<<i))!=0)count++;
-            }
-            if(count%3==1)ans|=(1<<i);
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i+=3){
+            if(nums[i]!=nums[i-1])return nums[i-1];
         }
-        return ans;
+        return nums[nums.length-1];
     }
 }
