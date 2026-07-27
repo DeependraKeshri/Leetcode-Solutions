a696cdb1af7819fb149e7760a0237af4555e919d
Deependra Kumar <keshrideependra17@gmail.com>
2026-07-27 12:03:04 +0530

    Time: 0 ms (100%), Space: 43.6 MB (91.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,20 +3,19 @@ class Solution {
         int sr=0, er=matrix.length-1;
         while(sr<=er){
             int mr=sr+(er-sr)/2;
-            int sc=0, ec=matrix[0].length-1;
-            while(sc<=ec){
-                int mc=sc+(ec-sc)/2;
-                if(matrix[mr][mc]==target)return true;
-                else if(matrix[mr][mc]>target){
-                    ec=mc-1;
-                }else{
-                    sc=mc+1;
+            int sc=0, ec=matrix[mr].length-1;
+            if(target==matrix[mr][sc] || matrix[mr][ec]==target)return true;
+            else if(target<matrix[mr][sc])er=mr-1;
+            else if(target>matrix[mr][ec])sr=mr+1;
+            else{
+                while(sc<=ec){
+                    int mc=sc+(ec-sc)/2;
+                    if(target==matrix[mr][mc])return true;
+                    else if(target<matrix[mr][mc])ec=mc-1;
+                    else sc=mc+1;
                 }
+                return false;
             }
-            if(sc==matrix[mr].length)sc--;
-            if(sc==-1)sc=0;
-            if(matrix[mr][sc]>target)er=mr-1;
-            else sr=mr+1;
         }
         return false;
     }
