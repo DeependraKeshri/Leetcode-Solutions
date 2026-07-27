95e2fd70f6b42e1ec08d8fd05fee389c5c728e66
Deependra Kumar <keshrideependra17@gmail.com>
2026-07-27 10:26:34 +0530

    Time: 1 ms (100%), Space: 42.1 MB (100%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.java
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.java
@@ -1,34 +1,21 @@
 class Solution {
     public int largestInteger(int n, int s) {
-        if (s == 0) return 0;
-        if (s > n * 9) return -1;
+        if (s == 0) {
+            return 0;
+        }
+
+        if (s > n * 9) {
+            return -1;
+        }
+
         StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            sb.append('9');
-            n--;
+
+        for (int i = 0; i < n; i++) {
+            int digit = Math.min(9, s);
+            sb.append(digit);
+            s -= digit;
         }
-        while (true) {
-            int sum = 0;
-            for (int i = 0; i < sb.length(); i++) {
-                sum += sb.charAt(i) - '0';
-            }
-            if (sum == s) {
-                return Integer.parseInt(sb.toString());
-            }
-            int i = sb.length() - 1;
-            if (sb.charAt(i) == '0') {
-                while (i > 0 && sb.charAt(i) == '0') {
-                    i--;
-                }
-                sb.setCharAt(i, (char)(sb.charAt(i) - 1));
-                i++;
-                while (i < sb.length()) {
-                    sb.setCharAt(i, '9');
-                    i++;
-                }
-            } else {
-                sb.setCharAt(i, (char)(sb.charAt(i) - 1));
-            }
-        }
+
+        return Integer.parseInt(sb.toString());
     }
 }
