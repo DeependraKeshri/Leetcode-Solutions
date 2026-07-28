fe5d5c937b567370391c63d43ee010ed35c563c8
Deependra Kumar <keshrideependra17@gmail.com>
2026-07-28 09:52:18 +0530

    Time: 1 ms (100%), Space: 47.9 MB (60.71%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xor=0;
-        for(int i=0; i<nums.length; i++){
+        int n=nums.length;
+        for(int i=0; i<n; i++){
             xor^=nums[i];
         }
-        int rightmost=(xor&(xor-1))^xor;
-        int ans[]=new int[2];
-        for(int i=0; i<nums.length; i++){
-            if((nums[i]&rightmost)!=0)ans[0]^=nums[i];
-            else ans[1]^=nums[i];
+        int rt=(xor & (xor-1))^xor;
+        int a=0, b=0;
+        for(int i=0; i<n; i++){
+            if((nums[i] & rt)!=0)a^=nums[i];
+            else b^=nums[i];
         }
-        return ans;
+        return new int[]{a,b};
     }
 }
