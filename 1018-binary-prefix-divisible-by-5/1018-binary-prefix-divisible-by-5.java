9e7d107556c378b294233144996af3699ec95e29
Deependra Kumar <keshrideependra17@gmail.com>
2026-09-15 19:42:14 +0530

    Time: 4 ms (29.06%), Space: 47.8 MB (28.44%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.java
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.java
@@ -1,13 +1,12 @@
 class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
-        List<Boolean> ans = new ArrayList<>();
-        int mod = 0;   // store prefix modulo 5
-
-        for (int bit : nums) {
-            mod = (mod * 2 + bit) % 5;   // update only the remainder
-            ans.add(mod == 0);
+        List<Boolean> list=new ArrayList<>();
+        long num=0;
+        for(int i=0; i<nums.length; i++){
+            num=(num*2+nums[i])%5;
+            if(num%5==0)list.add(true);
+            else list.add(false);
         }
-
-        return ans;
+        return list;
     }
 }
