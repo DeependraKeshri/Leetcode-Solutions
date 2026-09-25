ed9658bb1fa14b154b036ca6146666d52db29770
Deependra Kumar <keshrideependra17@gmail.com>
2026-09-25 13:42:42 +0530

    Time: 7 ms (9.88%), Space: 48.7 MB (5.4%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String intToRoman(int num) {
+        // HashMap<Integer, Character> mp=new HashMap<>();
+        // mp.put(1, 'I');
+        // mp.put(5, 'V');
+        // mp.put(10, 'X');
+        // mp.put(50, 'L');
+        // mp.put(100, 'C');
+        // mp.put(500, 'D');
+        // mp.put(1000, 'M');
+        String s="";
+        while(num>0){
+            if(num>=1000){
+                s+="M";
+                num-=1000;
+            }else if(num>=900){
+                s+="CM";
+                num-=900;
+            }else if(num>=500){
+                s+="D";
+                num-=500;
+            }else if(num>=400){
+                s+="CD";
+                num-=400;
+            }else if(num>=100){
+                s+="C";
+                num-=100;
+            }else if(num>=90){
+                s+="XC";
+                num-=90;
+            }else if(num>=50){
+                s+="L";
+                num-=50;
+            }else if(num>=40){
+                s+="XL";
+                num-=40;
+            }else if(num>=10){
+                s+="X";
+                num-=10;
+            }else if(num>=9){
+                s+="IX";
+                num-=9;
+            }else if(num>=5){
+                s+="V";
+                num-=5;
+            }else if(num>=4){
+                s+="IV";
+                num-=4;
+            }else{
+                s+="I";
+                num--;
+            }
+        }
+        return s;
+    }
+}
